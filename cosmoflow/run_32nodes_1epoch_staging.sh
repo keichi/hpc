54718b97d2901901dc2a9444d407b14fc239e8ef
Keichi Takahashi <hello@keichi.dev>
2025-02-27 10:04:26 +0900

    Add job scripts for Pegasus

---
+++ cosmoflow/run_32nodes_1epoch_staging.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -q gen_M
+#PBS -A INSITU
+#PBS -T openmpi
+#PBS -l elapstim_req=00:20:00
+#PBS -v NQSV_MPI_VER=nvhpc-hpcx-cuda11/24.5
+#PBS -b 32
+
+cd ${PBS_O_WORKDIR}
+
+module load cuda/11.8.0
+module load cudnn/8.9.7/cuda11
+module load openmpi/$NQSV_MPI_VER
+
+source .venv/bin/activate
+
+mpirun ${NQSV_MPIOPTS} -np 32 -npernode 1 --bind-to none -x PATH -x LD_LIBRARY_PATH \
+python3 train.py -d --rank-gpu --mlperf --n-epochs 1 \
+    --data-dir /work/INSITU/share/cosmoUniverse_2019_05_4parE_tf_v2 \
+    --stage-dir /pmem \
+    --amp
